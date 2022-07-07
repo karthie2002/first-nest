8e4359798f99bd54f0eb5b6f9af84f821a4cc495
Akash <67492990+Akash-Manikandan@users.noreply.github.com>
2022-07-07 18:36:42 +0530

    Cors

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{ cors: true });
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-
       transform: true,
     }),
   );
